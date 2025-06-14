ba53467a3fcaadec2b1f7d4b6737f2e7b954ef47
Joao Carias de Franca <joaocariasdefranca@gmail.com>
2025-06-13 23:14:20 -0300

    listando redes a minha volta

---
+++ Jcf.Lab.WorkServ/SevWifiMonitor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SimpleWifi;
+
+class Program
+{
+    static void Main()
+    {
+        var wifi = new Wifi();
+
+        foreach (var accessPoint in wifi.GetAccessPoints())
+        {
+            Console.WriteLine($"SSID: {accessPoint.Name}");
+            Console.WriteLine($"Sinal: {accessPoint.SignalStrength}%");
+            Console.WriteLine($"Protegido: {accessPoint.IsSecure}");
+            Console.WriteLine("---------------------------");
+
+         
+            
+        }
+    }
+}
